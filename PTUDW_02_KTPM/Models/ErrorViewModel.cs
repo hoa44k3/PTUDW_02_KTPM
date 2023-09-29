91cec389e8edec33a127932700dd6d5b8ffd7f91
noname44k1 <thanh.cntt.dhv@gmail.com>
2023-09-29 10:29:57 +0700

    Add project files.

---
+++ PTUDW_02_KTPM/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PTUDW_02_KTPM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
