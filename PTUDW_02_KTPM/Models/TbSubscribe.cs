cd16193caa0a44843fa0a50c8e1333f158cbd752
noname44k1 <thanh.cntt.dhv@gmail.com>
2023-10-06 10:47:20 +0700

    buoi 5

---
+++ PTUDW_02_KTPM/Models/TbSubscribe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PTUDW_02_KTPM.Models;
+
+public partial class TbSubscribe
+{
+    public int SubscribeId { get; set; }
+
+    public string? Email { get; set; }
+
+    public DateTime? CreateDate { get; set; }
+}
---
+++ PTUDW_02_KTPM/ViewComponents/MenuTopViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PTUDW_02_KTPM.Models;
+
+namespace PTUDW_02_KTPM.ViewComponents
+{
+    public class MenuTopViewComponent : ViewComponent
+    {
+        private readonly HarmicContext _context;
+
+        public MenuTopViewComponent(HarmicContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var items = _context.TbMenus.Where(m => (bool)m.IsActive).OrderBy(m => m.Position).ToList();
+            return await Task.FromResult<IViewComponentResult>(View(items));
+        }
+    }
+}
